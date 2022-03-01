64e6f2125d954272c9cd26ec02c3feb4aa6dcce3
AmvIsion1248 <amvision1248@gmail.com>
2022-03-01 12:56:15 +0900

    study_220228 add

---
+++ study_220228/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\KimTaeHa\Desktop\Rust_studying\TH.Rust\study_220228\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\KimTaeHa\Desktop\Rust_studying\TH.Rust\study_220228\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ study_220228/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\KimTaeHa\Desktop\Rust_studying\TH.Rust\study_220228\hello_world\target\debug\hello_world.exe: C:\Users\KimTaeHa\Desktop\Rust_studying\TH.Rust\study_220228\hello_world\src\main.rs
