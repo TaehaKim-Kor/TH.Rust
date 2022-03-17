b8a8ed123a1821fd5e88dfb24f9ba513ef86c151
AmvIsion1248 <amvision1248@gmail.com>
2022-03-17 23:18:38 +0900

    control tutorial update

---
+++ study_220317/control_tutorial/src/main.rs
@@ -0,0 +1,12 @@
+fn main(){
+	let number : i8 = 62;
+	if number > 70 {
+		println!("Number is bigger than 70.");
+	}
+	else if number > 60 {
+		println!("Number is bigger than 60.");
+	}
+	else {
+		println!("Number is same or lower than 60.");
+	}
+}
